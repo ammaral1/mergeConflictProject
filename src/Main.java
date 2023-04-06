126150ab4ab0e8a780c786bc126b8dcf053f50b9
ammaral1 <ammar.alqaraghuli@gmail.com>
2023-04-06 14:35:08 -0400

    feature2 added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Feature2 f2=new Feature2();
+        f2.B="1234";
+
     }
 }
